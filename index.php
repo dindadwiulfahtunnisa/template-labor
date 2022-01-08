717179474e1857ade00a8dd82f685b1311627c9c
dindadwiulfahtunnisa <dindadwiulfahh@gmail.com>
2022-01-08 23:43:56 +0700

    tambah data

--- index.php
+++ index.php
@@ -9,11 +9,15 @@
         include "layout/navbar.php";
         include "layout/sidebar.php";
 
-        if($halaman == "home"){
+        if($halaman == "home"){} else if($halaman == "newhome"){
+        include "pages/newhome.php";
             include "pages/home.php";
         } else if($halaman == "newhome"){
         include "pages/newhome.php";
-        }
+        } else if($halaman == "pelanggan/daftar"){
+        include "pages/pelanggan/daftar.php";
+        } else if($halaman == "pelanggan/tambah"){
+        include "pages/pelanggan/tambah.php";}
 
         include "layout/footer.php";
         include "layout/bottom.php";
