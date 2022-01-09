fb01d6b2151c9eeeff5afe98819abc4e7fc75bd6
dindadwiulfahtunnisa <dindadwiulfahh@gmail.com>
2022-01-09 16:40:41 +0700

    Form Tambah

---
+++ pages/pelanggan/daftar.php
@@ -0,0 +1,38 @@
+<main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Daftar Pelanggan</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Daftar Pelanggan</li>
+                        </ol>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <table class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th>Kode Pelanggan</th>
+                                                    <th>Nama Pelanggan</th>
+                                                    <th>Alamat</th>
+                                                    <th>Kota</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+
+                                            <tr>
+                                                <td>Kode Pelanggan</td>
+                                                <td>Nama Pelanggan</td>
+                                                <td>Alamat</td>
+                                                <td>Kota</td>
+                                            </tr>
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+   
+    
